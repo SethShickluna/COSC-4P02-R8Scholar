24e341c9e39201f281cf908215b089ca8c8d471c
Seth Shickluna <35981002+SethShicklunaPierce@users.noreply.github.com>
2021-02-11 11:21:25 -0500

    started working on the course page

--- r8scholar-frontend/src/pages/Course.js
+++ r8scholar-frontend/src/pages/Course.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import {Container, Row, Col} from 'react-bootstrap'; 
+
+const pageStyles={
+    margin: '0 auto', 
+    marginTop: '3%', 
+    width: '80%', 
+}; 
+
+const pageBreak = {
+    //this sets the margin for reviews and draws a line hovering under the titles 
+    marginBottom: '2%', 
+    height: '1px',
+    backgroundColor: '#ccc',
+    border: 'none',
+}
 
 export default class Course extends Component {6
     constructor(props) {
         super(props);
         //use state because react forces an update when it is modifed in some way 
         this.state = { //all the content that is gonna be retrieved from the api stored here locally
-            department: "", 
-            code: "", 
+            department: "COSC", 
+            code: "2P03", 
             avgRating: "", 
             reviews: null, //reviews would be an object 
             instructors: null, //another object 
@@ -38,8 +53,41 @@ export default class Course extends Component {6
 
     render() {
         return (
-            <div className="course-container">
-                Course page for {this.name}.
+            <div style={pageStyles}>
+                <Container fluid="md">
+                    <Row> {/* title row, includes course name and reviews*/}
+                        <Col sm={4}>
+                            <div name="title">
+                                <h1>{this.state.department + " " + this.state.code}</h1>
+                            </div>  
+                        </Col>
+                        <Col sm={7}>
+                            <div>
+                                <h1 style={{textAlign: 'center'}}>Reviews</h1>
+                            </div>
+                        </Col>
+                        <Col sm={1}/>
+                    </Row>
+                    {/*content row */}
+                    <Row>
+                        <Col>
+                            <div style={pageBreak}/>
+                        </Col>
+                    </Row>
+                    
+                    <Row>
+                        <Col sm={4}>
+                            <div name="title">
+                                <h4 style={{marginLeft: '7%'}}>Overall Ratings</h4>
+                            </div>  
+                        </Col>
+                        <Col sm={8}>
+                            <div>
+                              
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
