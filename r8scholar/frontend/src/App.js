4aea86e4f6be637730c47de0976523bfdca28669
Feesh404 <erikasklimusinas1@gmail.com>
2021-03-18 17:02:14 +0100

    added redirect to login when user's logged out

--- r8scholar/frontend/src/App.js
+++ r8scholar/frontend/src/App.js
@@ -1,7 +1,8 @@
 //node_modules
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React, {Component} from "react";
-import {render} from 'react-dom'; 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import cookies from "react-cookies";
 
 //css
 import "./assets/styles/bootstrap.min.css";
@@ -24,11 +25,10 @@ import Courses from "./pages/Courses";
 import Signup from "./pages/Signup";
 import Forum from "./pages/Forum";
 import PageNotFound from "./pages/PageNotFound";
-import Profile from "./pages/Profile"; 
-import Settings from "./pages/Settings"; 
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
 import Verification from "./pages/Verify";
 import SearchPage from "./pages/SearchPage";
-
 
 /*EXPLAINING HOW SPECIFYING COURSE, DEPARTMENTS, AND PROF NAMES WORK 
 the router specifies that a course can include a course name using a route like: /department/:coursename
@@ -37,12 +37,11 @@ then using that name we make the API call
 //----------------------------------------------------------------- */
 
 export default class App extends Component {
-    constructor(props){
-        super(props); 
-        
+    constructor(props) {
+        super(props);
     }
 
-    render(){
+    render() {
         return (
             <Router>
                 <Switch>
@@ -50,18 +49,24 @@ export default class App extends Component {
                     <Route path="/login" component={Login} history={history} />
                     <Route exact path="/signup" component={Signup} />
                     <Route path="/instructors" component={Instructors} />
-                    <Route path = "/search/:query" component={SearchPage}/>
+                    <Route path="/search/:query" component={SearchPage} />
                     <Route path="/verify" component={Verification} />
                     <Route exact path="/courses" component={Courses} />
                     <Route exact path="/profile" component={Profile} />
                     <Route exact path="/forum" component={Forum} />
                     <Route exact path="/account" component={Profile} />
-                    <Route exact path="/settings" component={Settings} />   
+                    <Route exact path="/settings" component={Settings} />
                     <Route exact path="/signout" component={Signout} />
                     <Route exact path="/departments" component={Departments} />
                     <Route path="/course/:courseName" component={Course} />
-                    <Route path="/instructor/:profName" component={Instructor} />
-                    <Route path="/department/:deptName" component={Department} />
+                    <Route
+                        path="/instructor/:profName"
+                        component={Instructor}
+                    />
+                    <Route
+                        path="/department/:deptName"
+                        component={Department}
+                    />
                     <Route path="*" component={PageNotFound} />
                 </Switch>
                 <Footer />
@@ -70,4 +75,4 @@ export default class App extends Component {
     }
 }
 
-render(<App/>, document.getElementById("root")); 
+render(<App />, document.getElementById("root"));
--- r8scholar/frontend/src/pages/Profile.js
+++ r8scholar/frontend/src/pages/Profile.js
@@ -1,49 +1,47 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Tab, Button } from 'reactstrap';
-import ReviewItem from '../components/ReviewItem';
-import SecondaryNav from '../components/SecondaryNav';
-import Tabs from 'react-bootstrap/Tabs';
-import cookie from 'react-cookies';
-import imageOne from '../assets/images/ben-sweet-2LowviVHZ-E-unsplash.jpg';
+import { Redirect } from "react-router";
+import { Container, Row, Col, Tab, Button } from "reactstrap";
+import ReviewItem from "../components/ReviewItem";
+import SecondaryNav from "../components/SecondaryNav";
+import Tabs from "react-bootstrap/Tabs";
+import cookie from "react-cookies";
+import imageOne from "../assets/images/ben-sweet-2LowviVHZ-E-unsplash.jpg";
 import $ from "jquery";
 import EditNicknameForm from "../components/EditNicknameForm";
 import EditPasswordForm from "../components/EditPasswordForm";
 
-
-
 const pageStyles = {
-    margin: '0 auto',
-    marginTop: '3%',
-    width: '90%',
+    margin: "0 auto",
+    marginTop: "3%",
+    width: "90%",
 };
 
 const buttonStyle = {
-    //height: '100vh',  
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white',
-    background: 'red',
-}
+    //height: '100vh',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    background: "red",
+};
 
 const pageBreak = {
-    //this sets the margin for reviews and draws a line hovering under the titles 
-    marginBottom: '2%',
-    marginTop: '2%',
-    height: '1px',
-    backgroundColor: '#dedede',
-    border: 'none',
-}
+    //this sets the margin for reviews and draws a line hovering under the titles
+    marginBottom: "2%",
+    marginTop: "2%",
+    height: "1px",
+    backgroundColor: "#dedede",
+    border: "none",
+};
 
 const tabStyle = {
-    paddingTop: '2.5%',
-}
+    paddingTop: "2.5%",
+};
 
 const imgStyle = {
-    width: '100%',
-    height: '200px',
-}
-
+    width: "100%",
+    height: "200px",
+};
 
 //code for submitting a profile image
 $(document).ready(function () {
@@ -52,34 +50,34 @@ $(document).ready(function () {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-                newImage = e.target.result; //set new image to result 
+                $(".profile-pic").attr("src", e.target.result);
+                newImage = e.target.result; //set new image to result
                 //this.setState({profile_picture : e.target.result}) //change the state object
-            }
+            };
 
             reader.readAsDataURL(input.files[0]);
         }
-    }
+    };
 
-
-    $(".file-upload").on('change', function () {
+    $(".file-upload").on("change", function () {
         readURL(this);
     });
 
-    $(".upload-button").on('click', function () {
+    $(".upload-button").on("click", function () {
         $(".file-upload").click();
     });
 });
-//end of this section of code. 
+//end of this section of code.
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
-        //use state because react forces an update when it is modifed in some way 
-        this.state = { //all the content that is gonna be retrieved from the api stored here locally
+        //use state because react forces an update when it is modifed in some way
+        this.state = {
+            //all the content that is gonna be retrieved from the api stored here locally
             user: null,
             reviews: null,
-        }
+        };
 
         this.verifyUser = this.verifyUser.bind(this);
         this.getAllReviews = this.getAllReviews.bind(this);
@@ -88,41 +86,45 @@ export default class Profile extends Component {
     }
 
     componentDidMount() {
-        fetch('/api/get-user' + '?email=' + cookie.load('email'))
+        fetch("/api/get-user" + "?email=" + cookie.load("email"))
             .then((response) => response.json())
             .then((data) => {
-                this.setState({ // the data.<variable> is just an example and will need to be changed to reflect what the backend returns 
+                this.setState({
+                    // the data.<variable> is just an example and will need to be changed to reflect what the backend returns
                     user: data,
                 });
             });
     }
 
-    changeConstant() {//change the nickname and get reviews from modified views.py
+    changeConstant() {
+        //change the nickname and get reviews from modified views.py
         this.constantName = this.state.user.nickname;
         this.getAllReviews(this.constantName);
     }
 
     getAllReviews(name) {
-        //this is just to have but will need to be slightly refactored 
+        //this is just to have but will need to be slightly refactored
         //make changes to the views to allow this functionality.
-        return fetch('/api/get-reviews' + '?nickname=' + this.constantName)
+        return fetch("/api/get-reviews" + "?nickname=" + this.constantName)
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    return null
+                    return null;
                 }
             })
             .then((data) => {
                 this.setState({
                     reviews: data,
-                    //loaded: true, 
-                })
+                    //loaded: true,
+                });
             });
     }
 
     verifyUser() {
-        var code = prompt("Enter the 6 digit code emailed to " + this.state.user.email);
+        var code = prompt(
+            "Enter the 6 digit code emailed to " + this.state.user.email
+        );
         const request = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -130,116 +132,174 @@ export default class Profile extends Component {
                 verification_code: code,
             }),
         };
-        fetch("/api/verify-user?email=" + this.state.user.email, request)
-            .then((response) => {
+        fetch("/api/verify-user?email=" + this.state.user.email, request).then(
+            (response) => {
                 if (response.ok) {
-                    //reload page 
+                    //reload page
                     alert("Success");
                     window.location.reload();
                 } else {
                     alert("Invalid Code!");
-                };
-            });
+                }
+            }
+        );
     }
 
-
     render() {
-        return (
-
+        console.log(cookie.load("isLoggeIn"));
+        return cookie.load("isLoggedIn") === "false" ? (
+            <Redirect to={"/login"} />
+        ) : (
             <div>
                 <SecondaryNav />
-                {this.state.user ?
+                {this.state.user ? (
                     <div style={pageStyles}>
                         <Container fluid="md">
                             <Row align="center">
                                 <Col>
-                                    {!this.state.user.is_verified ?
+                                    {!this.state.user.is_verified ? (
                                         <div>
-                                            <h2>Your account is not verfied!</h2>
-                                            <Button onClick={this.verifyUser} color="info" size="lg">
+                                            <h2>
+                                                Your account is not verfied!
+                                            </h2>
+                                            <Button
+                                                onClick={this.verifyUser}
+                                                color="info"
+                                                size="lg"
+                                            >
                                                 Verify Now
-                                </Button>
+                                            </Button>
                                         </div>
-                                        :
-                                        null}
+                                    ) : null}
                                 </Col>
                             </Row>
                             <Row style={{ marginTop: "3%" }}>
                                 <Col sm={4}>
                                     <div name="title">
-
                                         {/*insert image edit, accept image and input it */}
-                                        <h1 style={{ textAlign: 'center' }}>{this.state.user.nickname}</h1>
+                                        <h1 style={{ textAlign: "center" }}>
+                                            {this.state.user.nickname}
+                                        </h1>
                                     </div>
                                     <div style={pageBreak} /> {/* underline */}
-
                                     <div name="avg-rating-container">
                                         <div name="avg-rating-title">
-
-                                            <h4 style={{ textAlign: 'center' }}>Profile</h4>
+                                            <h4 style={{ textAlign: "center" }}>
+                                                Profile
+                                            </h4>
                                         </div>
-                                        <div style={{ textAlign: 'center' }} name="avg-rating">
-
-                                        </div>
+                                        <div
+                                            style={{ textAlign: "center" }}
+                                            name="avg-rating"
+                                        ></div>
                                     </div>
-
-                                    <div name="lecture-rating-container" style={{ marginTop: '25px' }}> {/*This container is for profile picture */}
+                                    <div
+                                        name="lecture-rating-container"
+                                        style={{ marginTop: "25px" }}
+                                    >
+                                        {" "}
+                                        {/*This container is for profile picture */}
                                         <div name="lecture-rating-title">
                                             {/*<h4 style={{ textAlign: 'center' }}> {Date()} </h4>*/}
                                         </div>
-                                        <div style={{ textAlign: 'center' }} name="lecture-rating">
-
-                                            <img className="profile-pic" src={imageOne} style={imgStyle} />
-
-
+                                        <div
+                                            style={{ textAlign: "center" }}
+                                            name="lecture-rating"
+                                        >
+                                            <img
+                                                className="profile-pic"
+                                                src={imageOne}
+                                                style={imgStyle}
+                                            />
                                         </div>
                                     </div>
-
-
                                     <div style={pageBreak} /> {/* underline */}
-
-
                                 </Col>
 
-                                <Col sm={7}> {/*change this section to one with 2 tabs , change the state form.*/}
-                                    <Tabs style={tabStyle} defaultActiveKey="reviews" transition={false}>
-
-                                        <Tab eventKey="reviews" title="Reviews" >
+                                <Col sm={7}>
+                                    {" "}
+                                    {/*change this section to one with 2 tabs , change the state form.*/}
+                                    <Tabs
+                                        style={tabStyle}
+                                        defaultActiveKey="reviews"
+                                        transition={false}
+                                    >
+                                        <Tab eventKey="reviews" title="Reviews">
                                             {/*<Button onClick ={this.changeConstant} >Reload</Button>*/}
-                                            {this.state.reviews !== null ?
-                                                this.state.reviews.map((item, index) =>
-                                                    (<ReviewItem id={index} key={"course-review" + index} reviewItem={item} />))
-                                                : (<div style={{ marginLeft: "20px" }}>No reviews yet! Be the first to leave one?</div>)
-                                            }
+                                            {this.state.reviews !== null ? (
+                                                this.state.reviews.map(
+                                                    (item, index) => (
+                                                        <ReviewItem
+                                                            id={index}
+                                                            key={
+                                                                "course-review" +
+                                                                index
+                                                            }
+                                                            reviewItem={item}
+                                                        />
+                                                    )
+                                                )
+                                            ) : (
+                                                <div
+                                                    style={{
+                                                        marginLeft: "20px",
+                                                    }}
+                                                >
+                                                    No reviews yet! Be the first
+                                                    to leave one?
+                                                </div>
+                                            )}
                                         </Tab>
 
-
-
-                                        <Tab eventKey="edit-profile" title="Change Password"> {/*Edit password form */}
-
-                                            {cookie.load('isLoggedIn') === "true" ?
-                                                (<EditPasswordForm> </EditPasswordForm>) //custom password change form
-                                                : (<div style={{ marginLeft: "20px" }}>Please log in or signup to edit your profile</div>)
-                                            }
+                                        <Tab
+                                            eventKey="edit-profile"
+                                            title="Change Password"
+                                        >
+                                            {" "}
+                                            {/*Edit password form */}
+                                            {cookie.load("isLoggedIn") ===
+                                            "true" ? (
+                                                <EditPasswordForm>
+                                                    {" "}
+                                                </EditPasswordForm> //custom password change form
+                                            ) : (
+                                                <div
+                                                    style={{
+                                                        marginLeft: "20px",
+                                                    }}
+                                                >
+                                                    Please log in or signup to
+                                                    edit your profile
+                                                </div>
+                                            )}
                                         </Tab>
 
-
-                                        <Tab eventKey="edit-nickname" title="Change Nickname"> {/*Edit nickname form */}
-
-                                            {cookie.load('isLoggedIn') === "true" ?
-                                                (<EditNicknameForm></EditNicknameForm>) //custom password change form
-                                                : (<div style={{ marginLeft: "20px" }}>Please log in or signup to edit your profile</div>)
-                                            }
-
-
+                                        <Tab
+                                            eventKey="edit-nickname"
+                                            title="Change Nickname"
+                                        >
+                                            {" "}
+                                            {/*Edit nickname form */}
+                                            {cookie.load("isLoggedIn") ===
+                                            "true" ? (
+                                                <EditNicknameForm></EditNicknameForm> //custom password change form
+                                            ) : (
+                                                <div
+                                                    style={{
+                                                        marginLeft: "20px",
+                                                    }}
+                                                >
+                                                    Please log in or signup to
+                                                    edit your profile
+                                                </div>
+                                            )}
                                         </Tab>
-
                                     </Tabs>
                                 </Col>
                             </Row>
                         </Container>
                     </div>
-                    : null}
+                ) : null}
             </div>
         );
     }
