cd35b138fc568d9f4a30c3d63f1c5314e8252e97
GrantNike <grant.nike001@protonmail.com>
2021-03-29 18:50:52 -0400

    Small change to remove_unverified.py

--- r8scholar/api/remove_unverified.py
+++ r8scholar/api/remove_unverified.py
@@ -12,10 +12,8 @@ import schedule
 #Deletes unverified user accounts that were not created today
 def delete_unverfied_users():
     CustomUser.objects.filter(is_verified = False).exclude(date_created = date.today()).delete()
-    #CustomUser.objects.filter(is_verified = False).delete()
     print("Removed Unverified")
 
-schedule.every(3).minutes.do(delete_unverfied_users)
 schedule.every().day.at("12:00").do(delete_unverfied_users)
 
 while True:
