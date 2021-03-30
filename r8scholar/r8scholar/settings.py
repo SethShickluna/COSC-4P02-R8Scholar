5a6492ae17f7923fbe9b19ffa444f00b66ddda12
James Sargant <shorinbonsai@gmail.com>
2021-03-30 16:56:55 -0400

    Prevent HTML rendering of API interface

--- r8scholar/r8scholar/settings.py
+++ r8scholar/r8scholar/settings.py
@@ -55,6 +55,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     ),  # 
 }
 
