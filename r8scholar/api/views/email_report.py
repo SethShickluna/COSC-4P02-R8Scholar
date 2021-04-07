317ae8b80fffd04ca0e9705dcdc8f4cb135a91ed
GrantNike <grant.nike001@protonmail.com>
2021-04-07 11:30:19 -0400

    fixed output of email sending scripts

--- r8scholar/api/views/email_report.py
+++ r8scholar/api/views/email_report.py
@@ -6,15 +6,18 @@ def email_r8scholar(review_id,report_description,numb_reports,email,nickname,sub
     smtp_server = "smtp.gmail.com"#Mail server(currently gmail)
     sender_email = "r8scholar@gmail.com" #R8scholar email
     password = "nuThutL!itr&7het" #R8scholar email password
-    message = "Subject: Reported Review \n"
-    message += "Review id: " + str(review_id)+"\n"
-    message += "Report description: "+str(report_description) +"\n"
-    message += "Number of reports on review: "+str(numb_reports)+"\n"
-    message += "Email of reviewer: "+str(email)+"\n"
-    message += "Nickname of reviewer: " +str(nickname)+"\n"
-    message += "Review subject: " +str(subject)+"\n"
-    message += "Review title: "+str(title)+"\n"
-    message += "Review content: "+str(content)+"\n"
+    message = f"""Subject: Reported Review
+
+
+    Review id: {review_id}
+    Report description: {report_description} 
+    Number of reports on review: {numb_reports} 
+    Email of reviewer: {email} 
+    Nickname of reviewer: {nickname} 
+    Review subject: {subject}
+    Review title: {title}
+    Review content: {content}"""
+    
     #Login to mail server and send email
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
